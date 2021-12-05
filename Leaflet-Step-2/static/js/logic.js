7b34a394e6e85cd65d9b2fb077226e842b53cafd
unknown <james.l.akerman@gmail.com>
2021-12-05 11:22:44 +1100

    leaflet 2 first draft

--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -1,24 +1,28 @@
 // Store our API endpoint inside queryUrl
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
 
-//  Define tile layer
-var baseMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/light-v10",
-  accessToken: API_KEY
+// Perform a GET request to the query URL
+d3.json(queryUrl).then(function(data) {
+  var earthquake_data = data.features;
+  getEarthquakeLayer(earthquake_data);
 });
 
-// Create our map
-var myMap = L.map("map", {
-  center: [
-    37.09, -108.71
-  ],
-  zoom: 5,
-  layers: [baseMap]
-});
+// Get the earthquake cricle layer
+function getEarthquakeLayer(data){
+  data.forEach(function(site){
+    var longitude = site.geometry.coordinates[0];
+    var latitude = site.geometry.coordinates[1];
+    var location = L.circle([latitude, longitude], {
+      fillOpacity: 0.75,
+      fillColor: getColor(site.properties.mag),
+      color: "#000000",
+      weight: "1",
+     // Adjust radius
+     radius: site.properties.mag * 25000
+     });
+    location.bindPopup("<h1>" + "Location: " + site.properties.place + "</h1>" + "<h1>" + "Magnitude: " + site.properties.mag + "</h1>").addTo(myMap);
+  });
+};
 
 function getColor(magnitude){
   switch (true) {
@@ -39,40 +43,40 @@ function getColor(magnitude){
   };
  };
 
-// Perform a GET request to the query URL
-d3.json(queryUrl).then(function(data) {
-  // Get the response
-  earthquake_data = data.features;
-  // Add circles to map
-  earthquake_data.forEach(function(site){
-  var longitude = site.geometry.coordinates[0];
-  var latitude = site.geometry.coordinates[1];
-  var i = L.circle([latitude, longitude], {
-    fillOpacity: 0.75,
-    fillColor: getColor(site.properties.mag),
-    color: "#000000",
-    weight: "1",
-   // Adjust radius
-   radius: site.properties.mag * 25000
-   // Add the pop up
-   }).bindPopup("<h1>" + "Location: " + site.properties.place + "</h1>" + "<h1>" + "Magnitude: " + site.properties.mag + "</h1>").addTo(myMap);
-  });
-
-  // Add a legend
-  var legend = L.control({position: 'bottomright'});
-  legend.onAdd = function (map) {
-    var div = L.DomUtil.create('div', 'info legend'),
-    magnitude_tiers = [0,1,2,3,4,5]
-    // loop through our density intervals and generate a label with a colored square for each interval
-    for (var i = 0; i < magnitude_tiers.length; i++) {
-        div.innerHTML +=
-            '<i style="background:' + getColor(magnitude_tiers[i]) + '"></i> ' +
-            magnitude_tiers[i] + (magnitude_tiers[i + 1] ? '&ndash;' + magnitude_tiers[i + 1] + '<br>' : '+');
-    }
-    return div;
-  };
-  legend.addTo(myMap);
+//  Define greyscale layer
+var greyscaleMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/light-v10",
+  accessToken: API_KEY
 });
+
+var darkMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/dark-v10",
+  accessToken: API_KEY
+});
+
+// Create our map
+var myMap = L.map("map", {
+  center: [
+    37.09, -108.71
+  ],
+  zoom: 5,
+  layers: [greyscaleMap, darkMap]
+});
+
+// Define a baseMaps object to hold our base layers
+var baseMaps = {
+  "Greyscale Map": greyscaleMap,
+  "Dark Map": darkMap
+};
+
 
 // Use this link to get the geojson data.
 var link = "static/js/GeoJSON/PB2002_boundaries.json";
@@ -86,4 +90,23 @@ d3.json(link).then(function(data) {
   // Add the the map
   fault_lines.addTo(myMap);
 });
+
+L.control.layers(baseMaps).addTo(myMap);
+
+
+// Add a legend
+var legend = L.control({position: 'bottomright'});
+legend.onAdd = function (map) {
+  var div = L.DomUtil.create('div', 'info legend'),
+  magnitude_tiers = [0,1,2,3,4,5]
+  // loop through our density intervals and generate a label with a colored square for each interval
+  for (var i = 0; i < magnitude_tiers.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(magnitude_tiers[i]) + '"></i> ' +
+            magnitude_tiers[i] + (magnitude_tiers[i + 1] ? '&ndash;' + magnitude_tiers[i + 1] + '<br>' : '+');
+    }
+    return div;
+};
+legend.addTo(myMap);
+
 
